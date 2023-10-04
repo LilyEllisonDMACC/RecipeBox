4e5c8efbc257ab95999832e93ad4737c932df802
Adam Reese <64161641+AdamMReese@users.noreply.github.com>
2023-10-04 13:29:07 -0500

    Updated Ingredient
    Updated the Ingredient model to work with the rest of the code I added.

--- src/model/Ingredient.java
+++ src/model/Ingredient.java
@@ -12,9 +12,6 @@
 
 package model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -29,22 +26,73 @@ public class Ingredient {
 	@GeneratedValue
 	@Column(name = "ID")
 	private int id;
+
 	@Column(name = "NAME")
 	private String name;
+
 	@Column(name = "QUANTITY")
 	private double quantity;
+
 	@Column(name = "UNIT")
 	private String unit;
 
+	// Default constructor
 	public Ingredient() {
 		super();
 	}
 
+	// Constructor with all fields
 	public Ingredient(int id, String name, double quantity, String unit) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.quantity = quantity;
 		this.unit = unit;
+	}
+
+	// Getter for id
+	public int getId() {
+		return id;
+	}
+
+	// Setter for id
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	// Getter for name
+	public String getName() {
+		return name;
+	}
+
+	// Setter for name
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	// Getter for quantity
+	public double getQuantity() {
+		return quantity;
+	}
+
+	// Setter for quantity
+	public void setQuantity(double quantity) {
+		this.quantity = quantity;
+	}
+
+	// Getter for unit
+	public String getUnit() {
+		return unit;
+	}
+
+	// Setter for unit
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+
+	// Override toString() method
+	@Override
+	public String toString() {
+		return "Ingredient [id=" + id + ", name=" + name + ", quantity=" + quantity + ", unit=" + unit + "]";
 	}
 }
